76495c63abd51d32c753edccdef4aa628e99a135
Chinmoy Chakraborty <chinmoychakraborty009@gmail.com>
2022-02-21 01:00:26 +0600

    Add the hamburger icon to the header

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,18 @@ import {NAME, MOTO} from '../constants/headerConstants' ;
 import {Link} from 'react-router-dom';
 
  class Header extends Component {
+  
+  hamBurgerMenuClicked(e) {
+      //TODO: There should be the fuctionality for passing data to parent to show/hide nav bar
+  }
+
+
   render() {
     return (
         <div class="header">
+        <a href="" class="icon" onClick={(e) => this.hamBurgerMenuClicked(e)}>
+            <i class="fa fa-bars"></i> 
+        </a>
         <Link to="/" class="logo">{NAME}</Link>
         <div class="header-right">
           <p>{MOTO}</p>
